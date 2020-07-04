9655ae28d8aa014a603c893dbbb4d646ccfd7439
rameezbadri <rameezbadri@usf.edu>
2020-07-03 23:09:05 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            DIS_Models person = new DIS_Models();
+            person.firstname = "Rameez";
+            person.lastname = "Badri";
+            person.address = new Address();
+            person.address.city = "Brandon";
+            person.address.street = "princeton lakes dr";
+            return View(person);
+        }
+        public IActionResult ISM6225()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/DIS_Models.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class DIS_Models
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+
+        public Address address { get; set; }
+    }
+    public class Address
+    {
+        public string street { get; set; }
+        public string city { get; set; }
+    }
+}
+
